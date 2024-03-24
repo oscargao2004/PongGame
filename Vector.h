35b6c773959990246adda28e87089e26df67221c
oscargao2004 <74120655+oscargao2004@users.noreply.github.com>
2024-03-24 01:02:33 -0400

    fixed mountain of errors in Vector class

--- Vector.h
+++ Vector.h
@@ -7,7 +7,7 @@ public:
 	int x;
 	int y;
 
-	Vector(int x, int y) : x(x), y(y), _gridLength(x), up {}
+	Vector(int x, int y) : x(x), y(y), _gridLength(x){}
 	Vector add(Vector vec);
 	Vector subtract(Vector vec);
 	bool isEqual(Vector vec);
